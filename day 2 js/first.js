b411d408fed26cc0efef81f2ba31c48db3e60512
Chandnahc <71308203+Chandnahc@users.noreply.github.com>
2021-08-04 17:58:31 +0530

    Add files via upload
    day 2 assignment

---
+++ day 2 js/first.js
@@ -0,0 +1,12 @@
+let a=["Ram", "Sita", "Ravan", "Krishna", "Kansh"];
+for(let i=4;i>=0;i--)
+{
+  console.log(a[i]+" ");
+}
+for(i=1;i<=50;i++)
+{
+  if(i%5==0)
+  {
+    console.log(i+" ");
+  }
+}
